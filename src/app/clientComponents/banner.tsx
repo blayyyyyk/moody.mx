4a8f0011949edfaa4afb9f486de705af5ed2ae47
Blake Moody <32070003+blayyyyyk@users.noreply.github.com>
2025-11-24 15:38:39 -0500

    fix for banner

--- src/app/clientComponents/banner.tsx
+++ src/app/clientComponents/banner.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 
 export default function Banner({ children, speed }: { children: string, speed: number}) {
     return (
-        <div className='absolute top-0 left-0 w-screen h-[calc(100vh_+_1px)] bg-transparent text-[10px] font-mono border-b-1 border-primary'>
+        <div className='absolute top-0 left-0 w-full h-[calc(100vh_+_1px)] bg-transparent text-[10px] font-mono border-b-1 border-primary'>
             <Marquee className="absolute origin-bottom-left left-0 translate-y-[-100%] bg-secondary border-t-1 border-primary" style={{ height: "auto", width: "100vh"}} direction="down" speed={speed} autoFill={true}>
                 <div className='rotate-90'>{children}</div>
             </Marquee>
@@ -13,7 +13,7 @@ export default function Banner({ children, speed }: { children: string, speed: n
             <Marquee className="absolute top-0 translate-y-[-200%] bg-secondary border-b-1 border-primary" direction="left" speed={speed} autoFill={true}>
                 <div className="rotate-180">{children}</div>
             </Marquee>
-            <Marquee className="absolute top-[100vh] translate-y-[-400%] bg-secondary border-t-1 border-primary" direction="right" speed={speed} autoFill={true}>
+            <Marquee className="absolute top-full translate-y-[-400%] bg-secondary border-t-1 border-primary" direction="right" speed={speed} autoFill={true}>
                 <div>{children}</div>
             </Marquee>
         </div>
